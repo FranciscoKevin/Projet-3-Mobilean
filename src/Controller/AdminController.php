5cf2c3898c794cebd9f1b23b25f62fb5c6ba6593
erguculu <erguculu@gmail.com>
2021-01-06 15:21:06 +0100

    Created connection of admin interface

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin", name="admin_")
+ * @IsGranted("ROLE_ADMIN")
  */
 class AdminController extends AbstractController
 {
